c3fa9fca38ae25e2838c38b6f530efc8b05f74b0
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-15 23:10:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,8 +16,16 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if (text.match(/\A[A-Z].*[.?!"']\z/))
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if (phone.match(/\b\d{3}-\d{3}-\d{4}\b/))
+    return true
+  else
+    return false
+  end
 end
